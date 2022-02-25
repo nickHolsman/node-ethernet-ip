d8836868a637693a257a367db983a57079c6f106
Nick <nick.holsman@kaiburrsystems.com>
2022-02-25 14:56:55 -0500

    added bit index override functionality to Param class

--- src/param/index.js
+++ src/param/index.js
@@ -5,7 +5,7 @@ const { CIP } = require("../enip");
 // Static Class Property - Tracks Instances
 let instances = 0;
 class Param extends EventEmitter {
-    constructor(controller, paramName, index = null) {
+    constructor(controller, paramName, bitIndex = null) {
         super();
         instances++;
 
@@ -13,21 +13,36 @@ class Param extends EventEmitter {
         this.value = null;
         this.isInput = false;
         this.isOutput = false;
+        this.inputIndex = null;
+        this.outputIndex = null;
+        this.input = null;
+        this.output = null;
+        this.bitIndex = bitIndex;
         this.timestamp = new Date();
         this.controller = controller;
         
-        // find param on controller
-        /* let paramIndex = this.controller.EDS.Params.findIndex(element => element.Data.Name == paramName);
-        if (paramIndex == -1) throw new Error(`Parameter ${paramName} not found!`); */
-
         // Determine if input/output
         let inputIndex = this.controller.state.inputs.findIndex(element => element.Name == this.name);
         let outputIndex = this.controller.state.outputs.findIndex(element => element.Name == this.name);
-        if (outputIndex > -1) this.isOutput = true;
-        if (inputIndex > -1) this.isInput = true;
+        if (outputIndex > -1) {
+            this.isOutput = true;
+            this.output = this.controller.state.outputs[outputIndex];
+        }
+        if (inputIndex > -1) {
+            this.isInput = true;
+            this.input = this.controller.state.inputs[inputIndex];
+        }
+
         if (outputIndex == -1 && inputIndex == -1) {
             throw new Error(`Parameter ${paramName} not found!`);
         }
+
+        // Throw error if incorret bit index
+        /* let inputBitSize = this.input.ByteSize*4;
+        let outputBitSize = this.output.ByteSize*4;
+        if (this.bitIndex !== null && (this.bitIndex > inputBitSize || this.bitIndex > outputBitSize)) {
+            throw new Error(`Parameter ${paramName} must have bit index between 0 !`);
+        } */
 
         // assign tag info to param
         this.controller.addTag(this,this.isInput,this.isOutput);
@@ -38,11 +53,29 @@ class Param extends EventEmitter {
 
     update(value) {
         if (!this.isOutput) throw new Error(`Parameter ${this.name} is not an output`);
+
+        // If param has overriden bit index than update via bitwise
+        if (this.bitIndex !== null) {
+            let newValue = this.value & ( 1 << this.bitIndex);
+            this.controller._setOutputByName(this.name,newValue);
+            return;
+        }
         this.controller._setOutputByName(this.name,value);
+        return;
     }
 
     changed(value) {
-        this.emit("Changed",value);
+        let newValue = value;
+
+        if (this.bitIndex !== null) {
+            newValue = (value & (1 << this.bitIndex)) == 0 ? false : true;
+        }
+
+        if (this.value == newValue) return;
+
+        this.value = newValue;
+        this.emit("Changed",newValue);
+        return;
     }
 
 }
